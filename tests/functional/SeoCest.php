1f806e626d0bd0d8eefa3114ffbba3e46899016d
RebOOter <rebooter8d@gmail.com>
2019-01-20 21:34:24 +0700

    [*] Changed implementation of SEO test

--- tests/functional/SeoCest.php
+++ tests/functional/SeoCest.php
@@ -1,15 +1,26 @@
 <?php
 
+use ParseCsv\Csv;
+
 class SeoCest
 {
-    public function checkSeoRules(FunctionalTester $I) {
-        $seo = $I->parseCSV('tests/_data/seo_example_wrong.csv');
-        foreach ($seo as $item) {
-            $I->amOnUrl($item['url']);
-            $title = $item['title'];
+    /**
+     * @dataProvider csvProvider
+     */
+    public function checkSeoRules(FunctionalTester $I, \Codeception\Example $seo) {
+            $I->amOnUrl($seo['url']);
+            $title = $seo['title'];
             $I->seeElement(".//title[text() = '$title']");
-            $description = $item['meta description'];
+            $description = $seo['meta description'];
             $I->seeElement(".//meta[@name = 'description' and @content = '$description']");
-        }
+    }
+
+    protected function csvProvider() {
+        $filePath = 'tests/_data/seo_example_wrong.csv';
+
+        $csv = new Csv();
+        $csv->offset = 1;
+        $csv->parse($filePath);
+        return $csv->data;
     }
 }
